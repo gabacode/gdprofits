ab7701afb15517122c93fa2397381536a1419ffc
Gabriele Scalici <gaba41@gmail.com>
2021-08-13 17:44:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function App() {
     <div className="wrapper">
       <div className="main">
         <img className="logo" src={logo} alt="logo"/>
-        <input type="number" name="profits" min="0" onChange={handleChange} />
+        <input type="number" name="profits" min="0" onChange={handleChange} placeholder="Inserisci importo"/>
         <p>€{dailyProfit} daily</p>
         <p>€{(dailyProfit * 7).toFixed(2)} per week</p>
         <p>~€{(dailyProfit * 30).toFixed(2)} per month</p>
