c1ba6eda1e791b4bffba5b276bcf23fdb96fcc6c
Gabriele Scalici <gaba41@gmail.com>
2021-09-07 15:17:28 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import logo from './images/logo.png'
 export default function App() {
 
   function handleChange(e) {
-    setDaily((e.target.value / 100 * 0.714).toFixed(2));
+    setDaily((e.target.value / 100 * 0.33).toFixed(2));
   }
   const [dailyProfit, setDaily] = useState(0.00);
   return (
@@ -13,11 +13,11 @@ export default function App() {
       <div className="main">
         <img className="logo" src={logo} alt="logo"/>
         <input type="number" name="profits" min="0" onChange={handleChange} placeholder="Inserisci importo"/>
-        <p>€{dailyProfit} daily</p>
-        <p>€{(dailyProfit * 7).toFixed(2)} per week</p>
+        <p>~€{dailyProfit} daily</p>
+        <p>~€{(dailyProfit * 7).toFixed(2)} per week</p>
         <p>~€{(dailyProfit * 30).toFixed(2)} per month</p>
-        <p>€{(dailyProfit * 365).toFixed(2)} per year</p>
-        <small>based on 0.714% daily interest</small>
+        <p>~€{(dailyProfit * 365).toFixed(2)} per year</p>
+        <small>based on 10% monthly interest</small>
         <small>made by <a href="https://github.com/gabacode" target="_blank" rel="noreferrer">@gabacode</a></small>
       </div>
     </div>
